b7f1e3f9e37336b6bd26f960fd564f7fbb647962
Akhila Srinagula <89111745+AkhilaSrinagula@users.noreply.github.com>
2021-11-16 01:28:52 -0600

    initialize app

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// TODO: Include packages needed for this application
+//Packages needed for this application
 const fs = require("fs");
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
@@ -6,9 +6,9 @@ const generateMarkdown = require("./utils/generateMarkdown");
 const promptUser = () => {
   return inquirer.prompt(questions);
 };
-// TODO: Create an array of questions for user input
+// An array of questions for user input
 const questions = [
-  {
+  { 
     message: "Enter the title or Name of your project? (Required)",
     type: "input",
     name: "title",
@@ -71,7 +71,7 @@ const questions = [
   },
 ];
 
-// TODO: Create a function to write README file
+// Function to write README file
 function writeToFile(fileName, data) {
     fs.writeFile(fileName, data, (err) => {
         if(err) throw err
@@ -79,7 +79,7 @@ function writeToFile(fileName, data) {
     })
 }
 
-// TODO: Create a function to initialize app
+// Function to initialize app
 function init() {
     inquirer.prompt(promptUser)
     .then(function (userInput) {
@@ -87,6 +87,6 @@ function init() {
         writeToFile("README.md", generateMarkdown(userInput));
     });
 };
-
+console.log('Hello Node!');
 // Function call to initialize app
 init();
