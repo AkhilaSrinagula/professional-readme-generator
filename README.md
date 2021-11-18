d6bacb079fe63e3fd3f06efc1bf0efd24e3e694c
Akhila Srinagula <89111745+AkhilaSrinagula@users.noreply.github.com>
2021-11-18 02:32:17 -0600

    validate questions

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# professional-readme-generator
+# Akhila
+  ## License
+  [![MIT License](https://img.shields.io/apm/l/atomic-design-ui.svg?)](https://github.com/tterb/atomic-design-ui/blob/master/LICENSEs)
+
+  ## Table of Contents 
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contribution](#contribution)
+  - [Testing](#testing)
+  - [Additional Info](#additional-info)
+
+
+  ## Description:
+  This project help the user to generate a readme
+
+  ## Installation:
+  First install node init -y
+  ## Usage:
+  after typing node index.js the command line provides few questions which needs to be answered
+  ## License:
+  undefined
+  ## Contribution:
+  There is no contribution required as of now
+  ## Testing:
+  enter index.js
+
+  ## Additional Info:
+  - Github: [akhilasrinagula](https://github.com/akhilasrinagula)
+  - Email: undefined
--- index.js
+++ index.js
@@ -3,25 +3,47 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
-const promptUser = () => {
-  return inquirer.prompt(questions);
-};
+
 // An array of questions for user input
 const questions = [
-  { 
+  {
     message: "Enter the title or Name of your project? (Required)",
     type: "input",
     name: "title",
+    validate: (titleInput) => {
+      if (titleInput) {
+        return true;
+      } else {
+        console.log("Please enter the title or Name of your project!");
+        return false;
+      }
+    },
   },
   {
-    message: "What is the Author's name?",
+    message: "What is the your name? (Required)",
     type: "input",
-    name: "title",
+    name: "name",
+    validate: (nameInput) => {
+      if (nameInput) {
+        return true;
+      } else {
+        console.log("Please enter your name!");
+        return false;
+      }
+    },
   },
   {
-    message: "Enter your GitHub Username?",
+    message: "Enter your GitHub Username? (Required)",
     type: "input",
-    name: "username",
+    name: "github",
+    validate: (gitHubInput) => {
+      if (gitHubInput) {
+        return true;
+      } else {
+        console.log("Please enter your name!");
+        return false;
+      }
+    },
   },
   {
     message: "Enter the GitHub URL for the project?",
@@ -29,62 +51,117 @@ const questions = [
     name: "repoURL",
   },
   {
-    message: "What is your email address?",
+    message: "What is your email address? (Required)",
     type: "input",
-    name: "email address",
+    name: "email",
+    validate : (emailInput) => {
+      if (emailInput) {
+        return true;
+      }else {
+        console.log ("Plaese provide your email address")
+      }
+    }
   },
   {
     message: "Enter a brief description of your project?",
     type: "input",
     name: "description",
+    validate : (descriptionInput) => {
+      if (descriptionInput) {
+        return true;
+      }else {
+        console.log ("Plaese provide the description of your project")
+      }
+    }
   },
   {
     message: "Choose the license that applies to the Project?",
     type: "list",
     choices: ["Apache", "CC", "GPLv3", "GPLv2", "ISC", "MIT", "WTFPL", "None"],
-    name: "license",
+    name: "licensing",
+    validate: (licensingList) => {
+      if (licensingList) {
+        return true;
+      } else {
+        console.log("Please choose the license that applies to the Project!");
+        return false;
+      }
+    },
   },
   {
-    message: "What command should be run to install dependencies?",
+    message: "How do you install your project? (Required)",
     type: "input",
     name: "installation",
+    validate: (installationInput) => {
+      if (installationInput) {
+        return true;
+      }else {
+        console.log("Please provide how to install the project")
+      }
+    }
   },
   {
-    message: "Enter any test instruction for the project?",
+    message: "Enter any test instruction for the project? (Required",
     type: "input",
-    name: "tests",
+    name: "testing",
+    validate: (testingInput) => {
+      if (testingInput) {
+        return true;
+      } else {
+        console.log("Please provide any test instruction for the project!");
+        return false;
+      }
+    },
+
   },
-  {
-    message: "Enter the instructions on how to use the project?",
-    type: "input",
-    name: "usage",
+{
+  message: "Enter the instructions on how to use the project? (Required)",
+  type: "input",
+  name: "usage",
+  validate: (usageInput) => {
+    if (usageInput) {
+      return true;
+    } else {
+      console.log("Please provide the instructions on how to use the project!");
+      return false;
+    }
   },
+},
   {
     message: "What is the copyright year?",
     type: "input",
     name: "year",
   },
   {
-    message: "What needs to be known about contributing to the Project?",
+    message: "What needs to be known about contributing to the Project? (Required)",
     type: "input",
-    name: "contributing",
+    name: "contribution",
+    validate: (contributionInput) => {
+      if (contributionInput) {
+        return true;
+      } else {
+        console.log("Please provide the information on how to contribute to the project")
+      }
+    }
   },
 ];
 
 // Function to write README file
 function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, (err) => {
-        if(err) throw err
-        console.log("Success! Information transferred to the README!")
-    })
+  fs.writeFile(fileName, data, (err) => {
+    if (err) throw err;
+    console.log("Success! Check out README.md in this directory to see it");
+  });
 }
 
 // Function to initialize app
-promptUser()
-.then(data => {console.log('Done!'); return data})
-    .then(data => {return generateMarkdown(data)})
-        .then(writeReadme => {
-            return writeToFile(writeReadme);
-        })
-        .catch(error => {console.log(error + "An error has popped up!")
-    })
+function init() {
+  inquirer.prompt(questions)
+  .then(function (userInput) {
+      console.log(userInput)
+      writeToFile("README.md", generateMarkdown(userInput));
+  });
+};
+
+// Function call to initialize app
+init();
